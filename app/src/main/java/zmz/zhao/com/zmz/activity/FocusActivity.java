c5a340c1a6f3e4becf4650e650fb94c7d2acf949
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-01-23 19:53:04 +0800

    md

--- app/src/main/java/zmz/zhao/com/zmz/activity/FocusActivity.java
+++ app/src/main/java/zmz/zhao/com/zmz/activity/FocusActivity.java
@@ -2,15 +2,29 @@ package zmz.zhao.com.zmz.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 import com.bw.movie.R;
 
+import butterknife.BindView;
+import butterknife.OnCheckedChanged;
+
 
 public class FocusActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_focus);
     }
+    @OnCheckedChanged(R.id.mine_radio)
+    public void OnClickChanged(RadioGroup group, int checkedId){
+        if (checkedId == R.id.mine_movie){
+
+        }
+    }
+
 }
