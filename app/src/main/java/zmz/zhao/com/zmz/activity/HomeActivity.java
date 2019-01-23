895987cc383a7afca895d4884d7973b616afaace
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-01-23 10:38:41 +0800

    dengl

---
+++ app/src/main/java/zmz/zhao/com/zmz/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package zmz.zhao.com.zmz.activity;
+
+import zmz.zhao.com.zmz.R;
+
+public class HomeActivity extends BaseActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
--- app/src/main/java/zmz/zhao/com/zmz/activity/MainActivity.java
+++ app/src/main/java/zmz/zhao/com/zmz/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package zmz.zhao.com.zmz.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.view.animation.AlphaAnimation;
@@ -10,16 +11,27 @@ import butterknife.BindView;
 import zmz.zhao.com.zmz.R;
 
 public class MainActivity extends BaseActivity {
+
     private int time = 3;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            SharedPreferences sharedPreferences = getSharedPreferences("movie", MODE_PRIVATE);
             time--;
             if (time > 0) {
+
                 handler.sendEmptyMessageDelayed(0, 1000);
 
             } else {
+
+                boolean flag = sharedPreferences.getBoolean("flag", false);
+
+                if (flag){
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return;
+                }
                 startActivity(new Intent(MainActivity.this, IntroductionActivity.class));
                 finish();
             }
@@ -35,11 +47,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+
+
         if (time != 0) {
             handler.sendEmptyMessageDelayed(0, 1000);
-            AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
-            alphaAnimation.setDuration(3500);
-            main.startAnimation(alphaAnimation);
         }
     }
 
