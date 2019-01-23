fbe3014788fa40df7937d7e3c4954bcabbc6873e
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-01-23 14:41:16 +0800

    fragment

--- app/src/main/java/zmz/zhao/com/zmz/activity/HomeActivity.java
+++ app/src/main/java/zmz/zhao/com/zmz/activity/HomeActivity.java
@@ -1,8 +1,29 @@
 package zmz.zhao.com.zmz.activity;
 
-import zmz.zhao.com.zmz.R;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
-public class HomeActivity extends BaseActivity{
+import butterknife.BindView;
+import zmz.zhao.com.zmz.R;
+import zmz.zhao.com.zmz.fragment.HomeFragment;
+import zmz.zhao.com.zmz.fragment.MineFragment;
+import zmz.zhao.com.zmz.fragment.MovieFragment;
+
+public class HomeActivity extends BaseActivity {
+
+
+    @BindView(R.id.frame)
+    FrameLayout frame;
+    @BindView(R.id.home_radio)
+    RadioGroup home_radio;
+
+    HomeFragment homeFragment;
+    MovieFragment movieFragment;
+    MineFragment mineFragment;
+    private FragmentManager manager;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_home;
@@ -10,6 +31,41 @@ public class HomeActivity extends BaseActivity{
 
     @Override
     protected void initView() {
+        homeFragment = new HomeFragment();
+        movieFragment = new MovieFragment();
+        mineFragment = new MineFragment();
+
+        manager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.frame, homeFragment);
+        transaction.commit();
+
+        home_radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    //主页
+                    case R.id.homePage:
+                        FragmentTransaction transaction1 = manager.beginTransaction();
+                        transaction1.replace(R.id.frame,homeFragment);
+                        transaction1.commit();
+                        break;
+                    //影院
+                    case R.id.homeMovie:
+                        FragmentTransaction transaction2 = manager.beginTransaction();
+                        transaction2.replace(R.id.frame,movieFragment);
+                        transaction2.commit();
+                        break;
+                    //我的
+                    case R.id.mine:
+                        FragmentTransaction transaction3 = manager.beginTransaction();
+                        transaction3.replace(R.id.frame,mineFragment);
+                        transaction3.commit();
+                        break;
+                }
+            }
+        });
 
     }
 
---
+++ app/src/main/java/zmz/zhao/com/zmz/fragment/MovieFragment.java
@@ -0,0 +1,22 @@
+package zmz.zhao.com.zmz.fragment;
+
+import android.view.View;
+
+import zmz.zhao.com.zmz.R;
+
+public class MovieFragment extends BaseFragment{
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData(View view) {
+
+    }
+
+    @Override
+    public int getContent() {
+        return R.layout.activity_movie;
+    }
+}
