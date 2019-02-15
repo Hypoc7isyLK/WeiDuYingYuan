187f19d3f7fb54bd7dd9846b87a499865a1295eb
淡意衬优柔 <lk18833433822@163.com>
2019-02-15 20:11:31 +0800

    优化scrollview

--- app/src/main/java/zmz/zhao/com/zmz/fragment/MovieFragment.java
+++ app/src/main/java/zmz/zhao/com/zmz/fragment/MovieFragment.java
@@ -1,7 +1,12 @@
 package zmz.zhao.com.zmz.fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
@@ -9,13 +14,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.bw.movie.R;
 import com.umeng.analytics.MobclickAgent;
 
@@ -24,7 +33,6 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import zmz.zhao.com.zmz.activity.ChooseActivity;
 import zmz.zhao.com.zmz.activity.CinemaActivity;
 import zmz.zhao.com.zmz.adapter.CinemaListAdapter;
 import zmz.zhao.com.zmz.bean.CinemaListBean;
@@ -54,6 +62,8 @@ public class MovieFragment extends BaseFragment {
     RecyclerView cinemaRecycleTjyy;
     @BindView(R.id.cinema_recycle_fjyy)
     RecyclerView cinemaRecycleFjyy;
+    @BindView(R.id.dingweitext)
+    TextView dingweitext;
 
     private CinemaListAdapter mCinemaListAdapter;
     private CinemaListPresenter mCinemaListPresenter;
@@ -62,10 +72,11 @@ public class MovieFragment extends BaseFragment {
     private String sessionId;
     FocusCinemaOffPresenter focusCinemaOffPresenter;
     FocusCinemaPresenter focusCinemaPresenter;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+
     @Override
     public void initView(View view) {
-        /*mSessionId = USER_INFO.getSessionId();
-        mUserId = USER_INFO.getUserId();*/
         mNearbyCinemaPresenter = new NearbyCinemaPresenter(new CinemalistCall());
         focusCinemaOffPresenter = new FocusCinemaOffPresenter(new FocusCinmaCall());
         focusCinemaPresenter = new FocusCinemaPresenter(new FocusOffCinmaCall());
@@ -90,15 +101,15 @@ public class MovieFragment extends BaseFragment {
         mCinemaListAdapter.setOnclicklitener(new CinemaListAdapter.Onclicklitener() {
             @Override
             public void success(int id, String address, String logo, String name) {
-                Log.e("lk","影院id"+id);
-                Log.e("lk","影院地址"+address);
-                Log.e("lk","影院logo"+logo);
-                Log.e("lk","影院name"+name);
-                Intent intent = new Intent(getActivity(),CinemaActivity.class);
-                intent.putExtra("id",id+"");
-                intent.putExtra("address",address);
-                intent.putExtra("logo",logo);
-                intent.putExtra("name",name);
+                Log.e("lk", "影院id" + id);
+                Log.e("lk", "影院地址" + address);
+                Log.e("lk", "影院logo" + logo);
+                Log.e("lk", "影院name" + name);
+                Intent intent = new Intent(getActivity(), CinemaActivity.class);
+                intent.putExtra("id", id + "");
+                intent.putExtra("address", address);
+                intent.putExtra("logo", logo);
+                intent.putExtra("name", name);
                 startActivity(intent);
             }
         });
@@ -107,7 +118,6 @@ public class MovieFragment extends BaseFragment {
             @Override
             public void success(int id, int state) {
                 if (USER_INFO != null) {
-                    Log.e("zmz","毁掉后"+state);
 
                     if (state == 1) {
                         focusCinemaOffPresenter.reqeust(userId, sessionId, id);
@@ -126,12 +136,12 @@ public class MovieFragment extends BaseFragment {
             //输入完成后，提交时触发的方法
             @Override
             public boolean onQueryTextSubmit(String query) {
-                if (query.isEmpty()){
+                if (query.isEmpty()) {
                     Toast.makeText(getContext(), "请输入查找内容！", Toast.LENGTH_SHORT).show();
                     return false;
                 }
 
-                Toast.makeText(getContext(), "搜索成功"+query, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "搜索成功" + query, Toast.LENGTH_SHORT).show();
 
                 return false;
             }
@@ -145,6 +155,41 @@ public class MovieFragment extends BaseFragment {
             }
         });
 
+        dingWei();
+
+    }
+
+    private void dingWei() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.READ_PHONE_STATE
+                }, 100);
+            } else {
+                mLocationClient = new LocationClient(getContext());
+                //声明LocationClient类
+                mLocationClient.registerLocationListener(myListener);
+                //注册监听函数
+                LocationClientOption option = new LocationClientOption();
+                option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+                //可选，是否需要位置描述信息，默认为不需要，即参数为false
+                //如果开发者需要获得当前点的位置信息，此处必须为true
+                option.setIsNeedLocationDescribe(true);
+                //可选，设置是否需要地址信息，默认不需要
+                option.setIsNeedAddress(true);
+                //可选，默认false,设置是否使用gps
+                option.setOpenGps(true);
+                //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+                option.setLocationNotify(true);
+                mLocationClient.setLocOption(option);
+                mLocationClient.start();
+            }
+        }
     }
 
     @Override
@@ -161,16 +206,15 @@ public class MovieFragment extends BaseFragment {
         cinemaRecycleTjyy.setLayoutManager(new LinearLayoutManager(getActivity(), OrientationHelper.VERTICAL, false));
         cinemaRecycleTjyy.setAdapter(mCinemaListAdapter);
 
-        if (USER_INFO != null){
+        if (USER_INFO != null) {
             userId = USER_INFO.getUserId();
             sessionId = USER_INFO.getSessionId();
             mCinemaListPresenter.reqeust(userId, sessionId, "1", "20");
-        }else {
+        } else {
             mCinemaListPresenter.reqeust(0, "", "1", "20");
         }
 
     }
-
 
 
     private void fjyy() {
@@ -182,14 +226,16 @@ public class MovieFragment extends BaseFragment {
         cinemaRecycleTjyy.setVisibility(View.GONE);//隐藏
         cinemaRecycleFjyy.setLayoutManager(new LinearLayoutManager(getActivity(), OrientationHelper.VERTICAL, false));
         cinemaRecycleFjyy.setAdapter(mCinemaListAdapter);
-        if (USER_INFO != null){
+        if (USER_INFO != null) {
             userId = USER_INFO.getUserId();
             sessionId = USER_INFO.getSessionId();
             mNearbyCinemaPresenter.reqeust(userId, sessionId, "1", "20");
-        }else {
+        } else {
             mNearbyCinemaPresenter.reqeust(0, "", "1", "20");
         }
     }
+
+
 
     private class CinemalistCall implements DataCall<Result<List<CinemaListBean>>> {
 
@@ -197,11 +243,12 @@ public class MovieFragment extends BaseFragment {
 
         @Override
         public void success(Result<List<CinemaListBean>> result) {
-            if (result.getStatus().equals("0000")){
+            if (result.getStatus().equals("0000")) {
                 mResult = result.getResult();
                 mCinemaListAdapter.reset(mResult);
             }
         }
+
         @Override
         public void fail(ApiException e) {
 
@@ -234,11 +281,11 @@ public class MovieFragment extends BaseFragment {
     private class FocusCinmaCall implements DataCall<Result> {
         @Override
         public void success(Result result) {
-            if (result.getStatus().equals("0000")){
-                if (cinemaTjyy.isChecked()){
+            if (result.getStatus().equals("0000")) {
+                if (cinemaTjyy.isChecked()) {
                     mCinemaListPresenter.reqeust(userId, sessionId, "1", "20");
                 }
-                if (cinemaFjyy.isChecked()){
+                if (cinemaFjyy.isChecked()) {
                     mNearbyCinemaPresenter.reqeust(userId, sessionId, "1", "20");
                 }
             }
@@ -253,11 +300,11 @@ public class MovieFragment extends BaseFragment {
     private class FocusOffCinmaCall implements DataCall<Result> {
         @Override
         public void success(Result result) {
-            if (result.getStatus().equals("0000")){
-                if (cinemaTjyy.isChecked()){
+            if (result.getStatus().equals("0000")) {
+                if (cinemaTjyy.isChecked()) {
                     mCinemaListPresenter.reqeust(userId, sessionId, "1", "20");
                 }
-                if (cinemaFjyy.isChecked()){
+                if (cinemaFjyy.isChecked()) {
                     mNearbyCinemaPresenter.reqeust(userId, sessionId, "1", "20");
                 }
             }
@@ -267,5 +314,20 @@ public class MovieFragment extends BaseFragment {
         public void fail(ApiException e) {
 
         }
+    }
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取地址相关的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+            String addr = location.getCity();    //获取详细地址信息
+            if (addr != null && addr != "") {
+                dingweitext.setText(addr);
+            }
+
+        }
+
     }
 }
